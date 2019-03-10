9166ee1c0f2ca47ea38e170749efc857c596debc
remi <sawdinocrisis@gmail.com>
2019-03-10 23:10:34 +0900

    update key

--- src/key.c
+++ src/key.c
@@ -7,10 +7,14 @@ static unsigned char keys[256] = {};
 
 void keyboard(unsigned char key, int x, int y);
 void keyboardUp(unsigned char key, int x, int y);
+void specialKey(int key, int x, int y);
+void specialKeyUp(int key, int x, int y);
 
 void keyInit(){
   glutKeyboardFunc(keyboard);
   glutKeyboardUpFunc(keyboardUp);
+  glutSpecialFunc(specialKey);
+  glutSpecialUpFunc(specialKeyUp);
   glutIgnoreKeyRepeat(GL_TRUE);
 }
 
@@ -26,4 +30,34 @@ void keyboard(unsigned char key, int x, int y){
 void keyboardUp(unsigned char key, int x, int y){
   printf("keyboardUp: \"%c\"(%#x)\n", key, key);
   keys[key] = 0;
+}
+
+void specialKey(int key, int x, int y){
+  if (key == GLUT_KEY_UP) {
+    keys[KEY_UP] = 1;
+  }
+  if (key == GLUT_KEY_DOWN) {
+    keys[KEY_DOWN] = 1;
+  }
+  if (key == GLUT_KEY_LEFT) {
+    keys[KEY_LEFT] = 1;
+  }
+  if (key == GLUT_KEY_RIGHT) {
+    keys[KEY_RIGHT] = 1;
+  }
+}
+
+void specialKeyUp(int key, int x, int y){
+  if (key == GLUT_KEY_UP) {
+    keys[KEY_UP] = 0;
+  }
+  if (key == GLUT_KEY_DOWN) {
+    keys[KEY_DOWN] = 0;
+  }
+  if (key == GLUT_KEY_LEFT) {
+    keys[KEY_LEFT] = 0;
+  }
+  if (key == GLUT_KEY_RIGHT) {
+    keys[KEY_RIGHT] = 0;
+  }
 }
